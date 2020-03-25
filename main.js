6e74036656e0a85776d0ce9512316f2b0750bb59
Ali Emre Taşkın <aliemre1990@hotmail.com>
2020-03-25 17:23:27 +0300

    changes

--- main.js
+++
@@ -1,9 +0,0 @@
-const yargs = require('yargs');
-
-const server = require('./server');
-const configuration = require('./configuration');
-
-var clientWatchMode = yargs.argv.clientWatchMode;
-var clientProdMode = yargs.argv.clientProdMode;
-
-server.initialize(clientProdMode, clientWatchMode);
