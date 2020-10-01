fd7ec6b62460435424d7d725fd4b6d8eff92a494
Ali Emre Taşkın <aliemre1990@hotmail.com>
2020-10-01 14:35:06 +0300

    Added new methods

--- playground.js
+++ playground.js
@@ -1,17 +1,38 @@
-function formatmessage(str, ...args) {
-    if (args.length) {
-        var t = typeof args[0];
-        var key;
-        var args2 = ("string" === t || "number" === t) ?
-            Array.prototype.slice.call(args)
-            : args[0];
+// function formatmessage(str, ...args) {
+//     if (args.length) {
+//         var t = typeof args[0];
+//         var key;
+//         var args2 = ("string" === t || "number" === t) ?
+//             Array.prototype.slice.call(args)
+//             : args[0];
 
-        for (key in args2) {
-            str = str.replace(new RegExp("\\{" + key + "\\}", "gi"), args2[key]);
-        }
-    }
+//         for (key in args2) {
+//             str = str.replace(new RegExp("\\{" + key + "\\}", "gi"), args2[key]);
+//         }
+//     }
 
-    return str;
+//     return str;
+// }
+
+// console.log(formatmessage('hello {0} {1}', 'ali', 'emre'));
+
+function myClass() {
+
+}
+myClass.prototype.fn = function () {
+    console.log(this.getValue());
+}
+myClass.prototype.getValue = function () {
+    return 'value';
 }
 
-console.log(formatmessage('hello {0} {1}', 'ali', 'emre'));
+function childClass() {
+
+}
+childClass.prototype = Object.create(myClass.prototype);
+childClass.prototype.getValue = function(){
+    return 'overrided value'
+}
+
+var obj = new childClass();
+obj.fn();
